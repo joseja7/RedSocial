049e8a52ae0f360662a7ea25dfeb92c57d6589b7
05719450 <05719450@DESKTOP-BJDVAFU>
2017-10-09 12:43:46 +0200

    Version 1.3 Permite creacion de usuarios (html incompleto) y su inserción en mLAB. Las claves de los usuarios se almacenan con md5

---
+++ RedSocial/src/modelo/Usuario.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Usuario {
+	private String nombre;
+	private String clave;
+	
+	//Constructor de pass
+	public Usuario(String nombre, String clave) {
+		this.nombre=nombre;
+		this.clave=clave;
+	}
+	
+	//Constructor sin pass
+	public Usuario(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	//Constructor vacio
+	public Usuario() {
+		super();
+	}	
+	
+	//Get atributos
+	public String getNombre() {
+		return this.nombre;
+	}
+	public String getClave() {
+		return this.clave;
+	}
+	//Set atributos
+	public void setNombre(String nuevoNombre) {
+		this.nombre=nuevoNombre;
+	}
+	public void setClave(String nuevaClave) {
+		this.clave=nuevaClave;
+	}
+}
