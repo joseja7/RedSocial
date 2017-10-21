8526f7526c5e35b9194d128a8e4d0f2b43e5e3df
JA <DSoftware@10.0.2.15>
2017-10-21 12:55:13 +0200

    Recuperacion de credenciales parte1

---
+++ IntraVita/src/main/java/com/intravita/proyectointranet/email/MailSender.java
@@ -0,0 +1,48 @@
+package com.intravita.proyectointranet.email;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+public class MailSender {
+	private static String userName = "atencion.cliente.dissw@gmail.com";
+	private static String password = "atencioncliente1234";
+	//Prueba5543165312
+
+	private static Session getSession() throws SQLException{
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+		
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		});
+		return session;
+	}
+	public void sendMail(String usuario, String pwd) throws Exception{
+		try {
+
+			Message message = new MimeMessage(getSession());
+			message.setFrom(new InternetAddress(userName));
+			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(usuario));
+			message.setSubject("DisSW Practica - Recupera tu contraseña");
+			message.setText("La contraseña para acceder a tu cuenta es:"+pwd);
+			Transport.send(message);
+
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
