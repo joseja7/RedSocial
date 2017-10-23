3c01f1fd5d62c8b68ef45979be7aea477178f111
jmdr7494 <DSoftware@DSoftware-PC>
2017-10-23 23:11:20 +0200

    Elaboración de un pequeño diccionario con palabras malsonantes, aplicado sobre el registro de nuevos usuarios, tanto en su nombre, como su correo.

--- IntraVita/src/main/java/com/intravita/proyectointranet/utlidades/utilidades.java
+++ IntraVita/src/main/java/com/intravita/proyectointranet/utlidades/utilidades.java
@@ -11,6 +11,10 @@ public class utilidades {
 	 * @return 
 	 * @throws Exception 
 	 */
+	
+	static //Vector con las "malasPalabras" que el sistema reconocerá
+	String [] diccionario = {"cabron", "cabronazo", "maricon", "gilipollas", "tonto", "capullo", "idiota", "fuck", "fucking", "huevon", "polla", "pollon", "coño"};
+	
 	public static void credencialesValidas(String nombre, String email, String pwd1, String pwd2) throws Exception {
 		if(nombre.equals("") ||email.equals("")|| pwd1.equals("")|| pwd2.equals(""))
 			throw new Exception ("Por favor rellene todos los campos");
@@ -19,6 +23,7 @@ public class utilidades {
 		if(email.length()<=extensionEmail.length())
 			throw new Exception("Email invalido");
 		String extension=email.substring(email.length()-extensionEmail.length(), email.length());
+		palabrasMalas(email, diccionario);
 		
 		if(!extension.equals(extensionEmail))
 			throw new Exception("Email invalido");
@@ -35,12 +40,16 @@ public class utilidades {
 		int posicion2=nombre.lastIndexOf('.');
 		if(posicion!=posicion2)
 			throw new Exception("Formato nombre invalido");
-		palabrasMalas(nombre);
+		palabrasMalas(nombre, diccionario);
 	}
-	public static void palabrasMalas(String nombre)throws Exception{
-		if(nombre.contains("cabron"))
-			throw new Exception("Formato nombre invalido");
+	
+	public static void palabrasMalas(String nombre, String [] diccionario)throws Exception{
+		for(int i=0;i<diccionario.length;i++) {
+			if(nombre.contains(diccionario[i]))
+				throw new Exception("Formato del nombre y/o email invalido, contiene palabras malsonantes");
+		}
 	}
+	
 	public static void seguridadPassword(String pwd) throws Exception{
 		if(pwd.length()<8)
 			throw new Exception("Password poco segura (minimo 8 caracteres, con numeros y letras)");
